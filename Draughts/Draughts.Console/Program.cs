8c68c62d37385ee38a3eb45b80437c0eba041e91
Daniel Baynton <daniel.baynton@hotmail.com>
2021-10-18 23:09:15 +0100

    Initial commit

---
+++ Draughts/Draughts.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Draughts.Console
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
