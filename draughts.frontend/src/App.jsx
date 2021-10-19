d45b7f9b35eae234716d7192c0f170b8273c7d44
Daniel Baynton <daniel.baynton@hotmail.com>
2021-10-19 23:15:50 +0100

    Added index page

--- draughts.frontend/src/App.jsx
+++ draughts.frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
+import Index from "./pages/Index";
 
 export default class App extends React.Component{
 	render(){
@@ -10,6 +11,7 @@ export default class App extends React.Component{
 				<main>
 					<Navbar/>
 					<Switch>
+						<Route path="/" component={Index}/>
 					</Switch>
 				</main>
 				<footer>
---
+++ draughts.frontend/src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Index extends React.Component{
+	render(){
+		return(
+			<div className="container">
+				<h1>Welcome to Draughts.</h1>
+				<p>Looks like I haven't bothered to make a landing page yet.</p>
+			</div>
+		);
+	}
+}
