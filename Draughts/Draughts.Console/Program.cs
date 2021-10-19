a7678be35af6f307ac549990711ae0f37d793dd8
Daniel Baynton <daniel.baynton@hotmail.com>
2021-10-19 01:27:51 +0100

    Added game logic and console interface

--- Draughts/Draughts.Console/Program.cs
+++ Draughts/Draughts.Console/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace Draughts.Console
+﻿using System.Linq;
+using Draughts.GameLogic;
+
+namespace Draughts.Console
 {
     internal class Program
     {
         private static void Main()
         {
-            System.Console.WriteLine("Hello World!");
+            var board = new Board();
+            while (board.Winner == -1)
+            {
+                System.Console.Clear();
+                PrintBoard(board);
+                System.Console.WriteLine($"\nPlayer to move: {board.NextPlayer}\n");
+                
+                System.Console.Write("Origin: ");
+                var origin = System.Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                System.Console.Write("Destination: ");
+                var destination = System.Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                board.TakeMove((origin[0], origin[1]), (destination[0], destination[1]));
+            }
+
+            System.Console.Clear();
+            System.Console.WriteLine($"Winner: {board.Winner}!");
+        }
+
+        private static void PrintBoard(Board board)
+        {
+            for (var y = 0; y < 8; y++)
+            {
+                if (y % 2 == 1) System.Console.Write("   ");
+                for (var x = 0; x < 4; x++)
+                {
+                    System.Console.Write($"{board.Tiles[y][x]}".PadRight(2));
+                    System.Console.Write("   ");
+                }
+                System.Console.Write("\n");
+            }
         }
     }
 }
---
+++ Draughts/Draughts.GameLogic/Board.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Draughts.GameLogic
+{
+    public class Board
+    {
+        /// <summary>
+        ///     Represents all of the playable tiles on the board.
+        ///     -1 = Nothing, 0 = Black man, 1 = White man, 2 = Black king, 3 = White king
+        /// </summary>
+        public int[][] Tiles { get; }
+        
+        /// <summary>
+        ///     The player whose turn it is to move
+        ///     0 = Black, 1 = White
+        /// </summary>
+        public int NextPlayer { get; private set; }
+        
+        /// <summary>
+        ///     A list of moves which the next player could make
+        /// </summary>
+        public List<((int, int), (int, int))> ValidMoves { get; private set; }
+
+        /// <summary>
+        ///     The player who won the game
+        ///     -1 = Currently playing, 0 = Black, 1 = White
+        /// </summary>
+        public int Winner { get; private set; }
+        
+        public Board()
+        {
+            Tiles = new[]
+            {
+                new [] { 0,  0,  0,  0 },
+                new [] { 0,  0,  0,  0 },
+                new [] { 0,  0,  0,  0 },
+                new [] { -1, -1, -1, -1 },
+                new [] { -1, -1, -1, -1 },
+                new [] { 1,  1,  1,  1 },
+                new [] { 1,  1,  1,  1 },
+                new [] { 1,  1,  1,  1 }
+            };
+            Winner = -1;
+            ValidMoves = GetValidMoves();
+        }
+
+        public bool TakeMove((int, int) origin, (int, int) destination)
+        {
+            var move = (origin, destination);
+            if (!ValidMoves.Contains(move)) return false;
+            
+            // Move the piece to the new tile
+            Tiles[destination.Item2][destination.Item1] = Tiles[origin.Item2][origin.Item1];
+            Tiles[origin.Item2][origin.Item1] = -1;
+
+            // If the move was a take
+            var yDifference = destination.Item2 - origin.Item2; 
+            if (Math.Abs(yDifference) == 2)
+            {
+                // Find the tile between the origin and destination and remove the piece
+                var halfYDifference = yDifference / 2;
+                Tiles[origin.Item2 + halfYDifference][origin.Item1 + halfYDifference] = -1;
+
+                // If there's another taking move return early
+                ValidMoves = GetValidMoves(true);
+                if (ValidMoves.Any()) return true;
+            }
+            
+            // Change the next player around and calculate the new valid moves
+            NextPlayer = 1 - NextPlayer;
+            ValidMoves = GetValidMoves();
+            CheckForWinner();
+            return true;
+        }
+
+        private List<((int, int), (int, int))> GetValidMoves(bool ignoreSingleMoves = false)
+        {
+            var singleMoves = new List<((int, int), (int, int))>();
+            var jumpingMoves = new List<((int, int), (int, int))>();
+
+            for (var y = 0; y < 8; y++)
+            {
+                for (var x = 0; x < 4; x++)
+                {
+                    var tile = Tiles[y][x];
+
+                    // If the tile is empty or doesn't belong to the player to move, skip it
+                    if (tile == -1 || tile % 2 != NextPlayer) continue;
+                    
+                    // Offset the value of X to account for the non-playable tile at the start of every other row
+                    var offsetX = x + y % 2;
+                    
+                    // If the tile is black or a white king
+                    if (tile % 2 == 0 || tile == 3)
+                    {
+                        // If there's a tile up and left
+                        if (y != 7 && offsetX != 0)
+                        {
+                            var newTile = Tiles[y + 1][offsetX - 1];
+                            if (newTile == -1) singleMoves.Add(((x, y), (offsetX - 1, y + 1)));
+                            
+                            // If the piece on the new tile is an opponent's piece and there's an empty tile to jump over it to
+                            else if (newTile % 2 != NextPlayer && x != 0 && y < 6 && Tiles[y + 2][x - 1] == -1)
+                            {
+                                jumpingMoves.Add(((x, y), (x - 1, y + 2)));
+                            }
+                        }
+                    
+                        // If there's a tile up and right
+                        if (y != 7 && offsetX != 4)
+                        {
+                            var newTile = Tiles[y + 1][offsetX];
+                            if (newTile == -1) singleMoves.Add(((x, y), (offsetX, y + 1)));
+                            
+                            // If the piece on the new tile is an opponent's piece and there's an empty tile to jump over it to
+                            else if (newTile % 2 != NextPlayer && x != 3 && y < 6 && Tiles[y + 2][x + 1] == -1)
+                            {
+                                jumpingMoves.Add(((x, y), (x + 1, y + 2)));
+                            }
+                        }
+                    }
+                    
+                    // If the tile is white or a black king
+                    if (tile % 2 == 1 || tile == 2)
+                    {
+                        // If there's a tile down and left
+                        if (y != 0 && offsetX != 0)
+                        {
+                            var newTile = Tiles[y - 1][offsetX - 1];
+                            if (newTile == -1) singleMoves.Add(((x, y), (offsetX - 1, y - 1)));
+                            
+                            // If the piece on the new tile is an opponent's piece and there's an empty tile to jump over it to
+                            else if (newTile % 2 != NextPlayer && x != 0 && y > 1 && Tiles[y - 2][x - 1] == -1)
+                            {
+                                jumpingMoves.Add(((x, y), (x - 1, y - 2)));
+                            }
+                        }
+                    
+                        // If there's a tile down and right
+                        if (y != 0 && offsetX != 4)
+                        {
+                            var newTile = Tiles[y - 1][offsetX];
+                            if (newTile == -1) singleMoves.Add(((x, y), (offsetX, y - 1)));
+                            
+                            // If the piece on the new tile is an opponent's piece and there's an empty tile to jump over it to
+                            else if (newTile % 2 != NextPlayer && x != 3 && y > 1 && Tiles[y - 2][x + 1] == -1)
+                            {
+                                jumpingMoves.Add(((x, y), (x + 1, y - 2)));
+                            }
+                        }
+                    }
+                }
+            }
+
+            // If there are any jumping moves, all single moves would be invalid.
+            return ignoreSingleMoves || jumpingMoves.Any() ? jumpingMoves : singleMoves;
+        }
+        
+        private void CheckForWinner()
+        {
+            if (ValidMoves.Any()) return;
+            Winner = 1 - NextPlayer;
+        }
+    }
+}
