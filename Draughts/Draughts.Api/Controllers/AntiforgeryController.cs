dafbb39090d7f6498cd077812cc7046e3f380748
Daniel Baynton <daniel.baynton@hotmail.com>
2021-10-20 17:29:15 +0100

    Added antiforgery controller

---
+++ Draughts/Draughts.Api/Controllers/AntiforgeryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Draughts.Api.Controllers
+{
+    [Route("antiforgery")]
+    public class AntiforgeryController : Controller
+    {
+        private readonly IAntiforgery _antiforgery;
+
+        public AntiforgeryController(IAntiforgery antiforgery)
+        {
+            _antiforgery = antiforgery;
+        }
+        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var tokens = _antiforgery.GetAndStoreTokens(HttpContext);
+            return StatusCode(200, tokens.RequestToken);
+        }
+    }
+}
