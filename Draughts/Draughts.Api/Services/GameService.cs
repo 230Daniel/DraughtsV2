4924fddd56b0fd82f8b9e89d1b094d51417214cd
Daniel Baynton <daniel.baynton@hotmail.com>
2022-01-14 20:44:36 +0000

    Added serverside comments

--- Draughts/Draughts.Api/Services/GameService.cs
+++ Draughts/Draughts.Api/Services/GameService.cs
@@ -13,19 +13,22 @@ namespace Draughts.Api.Services
     {
         private readonly IServiceProvider _serviceProvider;
         private readonly Random _random;
-        private readonly char[] _codeCharacters;
         private ConcurrentDictionary<string, IGame> _games;
 
         public GameService(IServiceProvider serviceProvider, Random random)
         {
             _serviceProvider = serviceProvider;
             _random = random;
-            _codeCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
             _games = new();
         }
         
         public string CreateGame(GameOptions options)
         {
+            // Create a new game depending on the type specified by the options
+            // The games must be requested from dependency injection now rather than
+            // in the GameService constructor because if we did that, only one game
+            // instance would be created per game type.
+            
             IGame game = options.GameType switch
             {
                 GameType.LocalMultiplayer => _serviceProvider.GetRequiredService<LocalMultiplayerGame>(),
@@ -33,6 +36,10 @@ namespace Draughts.Api.Services
                 _ => throw new ArgumentOutOfRangeException(nameof(options.GameType))
             };
             game.Options = options;
+            
+            // This program is multi-threaded so two games can theoretically be created at once.
+            // If this happened then both games could theoretically get the same game code, causing problems.
+            // The lock keyword ensures that only one thread can enter the block at once, solving this issue.
             
             lock (_games)
             {
@@ -55,21 +62,38 @@ namespace Draughts.Api.Services
 
         public void RemoveRedundantGames()
         {
+            // Games become "redundant" when they have ended, but they have no way of self-destructing.
+            // This method removes all redundant games and is called when a player leaves a game.
+            
             foreach (var redundantGame in _games.Where(x => x.Value.IsRedundant))
-            {
                 _games.TryRemove(redundantGame);
-            }
         }
 
         private string GetCode()
         {
             string code;
 
+            // Generate a random 6-letter random code repeatedly until one is found which no game is currently using.
+            
+            // The do ... while (condition) syntax is rare, but it's basically
+            // a while loop that always executes once before checking the condition.
+            
             do
             {
+                // Use a StringBuilder rather than a string for performance optimisation.
+                // A string will allocate an entire new string each time it is appended to,
+                // whereas a StringBuilder will not.
+                
                 var codeBuilder = new StringBuilder();
+
                 for (var i = 0; i < 6; i++)
-                    codeBuilder.Append(_codeCharacters[_random.Next(0, _codeCharacters.Length)]);
+                {
+                    // Append a random character A-Z to the code builder
+                    var ascii = _random.Next(65, 91);
+                    var character = (char) ascii;
+                    codeBuilder.Append(character);
+                }
+
                 code = codeBuilder.ToString();
             } 
             while (_games.Any(x => x.Key == code));
